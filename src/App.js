e9649f9c8cd7dd16fce62750c9dc9842e64e98e9
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-03-24 15:00:13 +0100

    Added test Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Test from './views/Test';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
     </div>
   );
 }
