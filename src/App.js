0cff267abeed862c90cfe0d50bcaa62ce987e789
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-03-28 15:32:31 +0200

    Added About us and funny text for #7

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import Test from './views/Test';
+import AboutUs from './views/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Test />} />
+        <Route path='/about' element={<AboutUs />} />
       </Routes>
     </div>
   );
---
+++ src/views/AboutUs.js
@@ -0,0 +1,42 @@
+export default function AboutUs() {
+  return (
+    <div className="App">
+      <h1>Om oss</h1>
+      <p>Airbean:s historia går tillbaka till 20,000 B.C när vår skapare Airius beaniclus lyfte sin nybryggda kanna kaffe för snabbt, och råkade kasta den ner i händerna på sin kompis. Detta ledde Airius till en häpnadsväckande upptäckt! Blandningen av luftigt kaffe och extremt snabb leverans blev en succé direkt. Sen dess har imperiet aldrig slutat växa. Den senaste utvecklingen hos Airbean har varit övergången från kastande till drönare, vilket har ökat effektiviteten och säkerheten hos våra kunder med 80000%! </p>
+      <p>Du är välkommen in att testa den godaste versionen av ditt favoritkaffe, med en helt unik tvist av leverans! Airbean lovar nu även en brännskade-fri upplevelse!</p>
+    
+      <h3>Airbean:s kaffe är:</h3>
+      <ul>
+        <li>Ekologiskt</li>
+        <li>Närproducerat</li>
+        <li>Veganskt</li>
+        <li>Laktosfritt</li>
+        <li>Sockerfritt</li>
+        <li>Fritt från färgämnen</li>
+        <li>Utan konserveringsmedel</li>
+        <li>Utan palmolja</li>
+        <li>Fairtrade</li>
+        <li>Nyckelhåls-märkt</li>
+        <li>Kaffe-fritt</li>
+        <li>Svanenmärkt</li>
+        <li>Gjort på åkta läder</li>
+      </ul>
+
+      <h5>Kan innehålla spår av: </h5>
+      <ul>
+        <li>Mjölk</li>
+        <li>Socker</li>
+        <li>Barnarbete</li>
+        <li>Vatten</li>
+        <li>Kaffe</li>
+        <li>Nyckelhål</li>
+        <li>Svanar</li>
+        <li>Palmolja</li>
+        <li>Nötter</li>
+        <li>Skaldjur</li>
+        <li>Barn</li>
+        <li>Gelatin</li>
+      </ul>
+    </div>
+  );
+}
