2b41b298773d0f21002841955c795db79ff63a9f
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-03-28 18:43:16 +0200

    components/Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import Menu from './views/Menu';
 import Test from './views/Test';
 import AboutUs from './views/AboutUs';
@@ -8,6 +9,7 @@ import AboutUs from './views/AboutUs';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Test />} />
         <Route path='/about' element={<AboutUs />} />
