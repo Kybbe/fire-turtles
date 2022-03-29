0a725edabb6bde6edeef9d5ba3f978c6642b9d5c
Nowaxial <maria.toledo@live.se>
2022-03-29 09:42:27 +0200

    Created Cart.js, added route to HOME

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+
 import Menu from './views/Menu';
 import Test from './views/Test';
 import AboutUs from './views/AboutUs';
+
 
 function App() {
   return (
@@ -14,6 +17,7 @@ function App() {
         <Route path="/" element={<Test />} />
         <Route path='/about' element={<AboutUs />} />
         <Route path='/menu' element={<Menu />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
     </div>
   );
