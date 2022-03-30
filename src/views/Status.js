8ba7e3030b753b48783be76637158647b674f3ef
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-03-30 15:29:54 +0200

    Update Status.js

--- src/views/Status.js
+++ src/views/Status.js
@@ -2,21 +2,21 @@ import Drone from '../assets/graphics/drone.svg';
 
 function Status() {
     return (
-        <div class="background">
-    <div class="ordernum">
-      <h4>Ordernummer: </h4>
-    </div>
-    <div class="drone">
-      <img src= {Drone} alt='Drone'/>
-    </div>
-    <div class="order">
-      <h1>Din beställning är på väg!</h1>
-    </div>
-    <div class="timer">
-      <p>13 minuter</p>
-    </div>
-    <button><h3>Ok, cool!</h3></button>
-  </div>
+        <div className="background">
+          <div className="ordernum">
+            <h4>Ordernummer: </h4>
+          </div>
+          <div className="drone">
+            <img src= {Drone} alt='Drone'/>
+          </div>
+          <div className="order">
+            <h1>Din beställning är på väg!</h1>
+          </div>
+          <div className="timer">
+            <p>13 minuter</p>
+          </div>
+          <button><h3>Ok, cool!</h3></button>
+        </div>
     )
 }
 
