4761a15afd2fc9bf0b78315ddd586d2e88a37a25
Nowaxial <maria.toledo@live.se>
2022-03-30 13:40:02 +0200

    Added status.js and imported Status to App.js

---
+++ src/views/Status.js
@@ -0,0 +1,26 @@
+import Drone from '../assets/graphics/drone.svg';
+
+function Status() {
+    return (
+        <div class="background">
+    <div class="ordernum">
+      <h4>Ordernummer: </h4>
+    </div>
+    <div class="drone">
+      <Drone />
+    </div>
+    <div class="order">
+      <h1>Din beställning är på väg!</h1>
+    </div>
+    <div class="timer">
+      <p>13 minuter</p>
+    </div>
+    <button><h3>Ok, cool!</h3></button>
+  </div>
+    )
+}
+<<<<<<< Updated upstream
+export default Status;
+=======
+export default Status;
+>>>>>>> Stashed changes
