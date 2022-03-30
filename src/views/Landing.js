cb4ba19947b789f9961a0e636a4b16118e39b9f2
nadiasGame <nadiabenkassems@gmail.com>
2022-03-30 19:42:54 +0200

    landing css

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,16 +1,18 @@
-import  Landings from'../assets/graphics/airbean-landing.svg'
+import  Landings from'../assets/graphics/airbean-landing.svg';
+import '../views/landing.css';
 
 
 
 function Landing() {
     return (
-        <div className="airbeanfront">
+        <div className="airbeanfront,sides">
         
 
-            <h2 className="a"> A </h2>
+            <h2 className="a">A</h2>
             <h2 className="air">AIR BEAN</h2>
             <p className="drone"> DRONEDELIVERED COFFE</p>
             <img src={Landings} alt="landings"></img>
+            
         </div>
     )
 }
