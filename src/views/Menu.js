eaf36ec10e7be7c6009a2689ad9dbdeb57a707b8
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-03-30 14:45:54 +0200

    menu html

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,6 +1,17 @@
+import Add from '../assets/graphics/add.svg';
+
 function Menu() {
     return (
-        <h1>Meny</h1>
+        <div className="menuInfo">
+        <h1>MENU</h1>
+
+        <h2 className="coffe"></h2>
+        <h2 className="price"></h2>
+        <p className="product"></p>
+        <img src={Add} alt="add"></img>
+
+        </div>
+
     )
 }
 
