380a8304817797db660bcc388bfc438321a1db25
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-03-30 11:50:00 +0200

    Added cart items and css +++ fixed spelling errors

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,27 @@
+import upArrow from '../assets/graphics/arrow-up.svg';
+import downArrow from '../assets/graphics/arrow-down.svg';
+
 function Cart() {
-    return(
-        <h1>Din beställning</h1>
-    )
+	return(
+		<div className="cart">
+			<h1>Din beställning</h1>
+
+			<ul className='cartList'>
+				<li className='cartItem'>
+					<div className='itemText'>
+						<h3 className='cartTitle'>Titel</h3>
+						<p className='cartPrice'>123 kr</p>
+					</div>
+					<div className="quantity">
+						<button className='arrow up'><img src={upArrow} alt="Up arrow"></img></button>
+						<p className='cartQuantity'>1</p>
+						<button className='arrow down'><img src={downArrow} alt="Down arrow"></img></button>
+					</div>
+				</li>
+			</ul>
+
+		</div>
+	)
 }
 
 export default Cart;
