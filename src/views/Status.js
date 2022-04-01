96c421d68ab6f4db4235c36091a0cf4615357bec
Nowaxial <maria.toledo@live.se>
2022-04-01 12:01:56 +0200

    Added fetch & some css to Status.js
    Added fetch for ETA and orderNr + some css to Status.js

--- src/views/Status.js
+++ src/views/Status.js
@@ -1,10 +1,38 @@
 import Drone from '../assets/graphics/drone.svg';
+import { useEffect, useState} from 'react';
+
+
 
 function Status() {
+  const [eta, setEta] = useState('')
+  const [orderNr, setOrderNr] = useState('')
+
+  useEffect(() => {
+    const url = "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/airbean/order"
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setEta(json.eta);
+        setOrderNr(json.orderNr)
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+
+}, []);
+
+
+
+
+
     return (
-        <div className="background backgroundOrange">
+        <div className="background-status">
           <div className="ordernum">
-            <h4>Ordernummer: </h4>
+            <h4>Ordernummer: {orderNr} </h4>
           </div>
           <div className="drone">
             <img src= {Drone} alt='Drone'/>
@@ -13,9 +41,9 @@ function Status() {
             <h1>Din beställning är på väg!</h1>
           </div>
           <div className="timer">
-            <p>13 minuter</p>
+            <h3>{eta} minuter</h3>
           </div>
-          <button><h3>Ok, cool!</h3></button>
+          <button className='button-status'><h3>Ok, cool!</h3></button>
         </div>
     )
 }
