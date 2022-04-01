661206c7def3cd7488e9102efea3633c8765a182
nadiasGame <nadiabenkassems@gmail.com>
2022-04-01 09:06:05 +0200

    h

---
+++ src/views/Status 2.js
@@ -0,0 +1,23 @@
+import Drone from '../assets/graphics/drone.svg';
+
+function Status() {
+    return (
+        <div className="background backgroundOrange">
+          <div className="ordernum">
+            <h4>Ordernummer: </h4>
+          </div>
+          <div className="drone">
+            <img src= {Drone} alt='Drone'/>
+          </div>
+          <div className="order">
+            <h1>Din beställning är på väg!</h1>
+          </div>
+          <div className="timer">
+            <p>13 minuter</p>
+          </div>
+          <button><h3>Ok, cool!</h3></button>
+        </div>
+    )
+}
+
+export default Status;
