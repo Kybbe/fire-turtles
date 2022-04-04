50b021b672726438fe94f74ec684da6376e53f53
Nadia ben kassem <90767449+nadiasGame@users.noreply.github.com>
2022-04-04 19:09:58 +0200

    klar

--- src/views/AboutUs.js
+++ src/views/AboutUs.js
@@ -1,6 +1,16 @@
+import Header from '../assets/graphics/graphics-header.svg';
+import Footer from '../assets/graphics/graphics-footer.svg';
+import Cortado from '../assets/graphics/eva-cortado.jpg';
+
+import '../App.css'
+
 export default function AboutUs() {
+ 
   return (
-    <div className="about backgroundBeige">
+   
+   
+    <div className="about backgroundBeige "> <img src={Header} alt="sides1" className=" Header"></img>
+      
       <h1 style={{textAlign: "center"}}>Om oss</h1>
       <p>Airbean:s historia går tillbaka till 20,000 B.C när vår skapare Airius beaniclus lyfte sin nybryggda kanna kaffe för snabbt, och råkade kasta den ner i händerna på sin kompis. Detta ledde Airius till en häpnadsväckande upptäckt! Blandningen av luftigt kaffe och extremt snabb leverans blev en succé direkt. Sen dess har imperiet aldrig slutat växa. Den senaste utvecklingen hos Airbean har varit övergången från kastande till drönare, vilket har ökat effektiviteten och säkerheten hos våra kunder med 80000%! </p>
       <p>Du är välkommen in att testa den godaste versionen av ditt favoritkaffe, med en helt unik tvist av leverans! Airbean lovar nu även en brännskade-fri upplevelse!</p>
@@ -36,7 +46,13 @@ export default function AboutUs() {
         <li>Skaldjur</li>
         <li>Barn</li>
         <li>Gelatin</li>
+        
       </ul>
-    </div>
+      <img src={Cortado} alt="sides2" className="Cortado"></img>
+      <h2>EVA CORTADO</h2>
+      <h1>VD & GRUNDARE</h1>
+      <img src={Footer} alt="sides2" className="Footer"></img>
+     
+    </div >
   );
 }
